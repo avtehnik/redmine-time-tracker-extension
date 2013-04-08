b517d27d8109ac77c29d7049425b1ad62ba3edfa
Vitaliy Pitvalo <vitaliy@ekreative.com>
2013-04-09 02:28:02 +0300

    add issues loader

--- js/loader.js
+++ js/loader.js
@@ -8,6 +8,17 @@ var Loader = function(o){
             onComplete: cb
         }).send();
     }
+    this.loadIssues = function(projectId,cb){
+       new Request.Redmine({
+            url: this.options.url+'/issues.json',
+            data:{
+                'project_id' : projectId,
+                'limit' : 100,
+                'key' : this.options.key
+            },
+            onComplete: cb
+        }).send();
+    }
     
     this.init = function(options){
         this.options = options;
--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 window.addEvent('domready',function(){
-    
+//  to reed http://www.redmine.org/projects/redmine/wiki/Rest_TimeEntries    
     DataLoader = new Loader({
         key:'0e96523a813ecf559ddba696a4f9549489aa8781',
         url:'http://avtehnik.m.redmine.org'
     });
 
-
-    window.projects = function(data){
-    
-        
-    }
-
     DataLoader.loadProjects(function(data){
         data.projects.each(function(p){
-            new Element('div',{
-                'html':p.name
-                }).inject(document.body);  
+            var project = new Element('div',{
+                'html':'<div>'+p.id+' '+p.name+'</div>'
+            }).inject(document.body);
+                  
+                  var ul = new Element('ul').inject(project); 
+                  
+            DataLoader.loadIssues(p.id , function(data){
+                data.issues.each(function(i){
+                    new Element('li',{
+                        'html':i.start_date+' '+i.subject
+                    }).inject(ul);  
+                })
+                console.log(data);
+            })
         })
         console.log(data);
     })
